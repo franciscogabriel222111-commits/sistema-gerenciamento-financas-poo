d8fdd2a0bfb81e0ce68f7d579efcd5f3884aa523
Francisco Gabriel Oliveira Alves <franciscogabriel222111@gmail.com>
2026-02-23 21:52:42 -0300

    atualizacao

--- src/main/java/com/shottz/ConexaoDB.java
+++ src/main/java/com/shottz/ConexaoDB.java
@@ -6,22 +6,19 @@ import java.sql.SQLException;
 
 public class ConexaoDB {
     
-    // 1. URL de Conexão:
-    // Se for local, costuma ser algo como: "jdbc:postgresql://localhost:5432/financas"
-    // Se for na nuvem (ex: Supabase), pegue a string JDBC no painel do banco.
-    private static final String URL = "jdbc:postgresql://localhost:5432/bd_financas"; // Substitua "localholt" pelo host do seu banco e "bd_financas" pelo nome do seu banco
     
-    // 2. Credenciais:
-    private static final String USER = "postgres"; // Usuário padrão
-    private static final String PASS = "shottz"; // Coloque a senha do banco
+    private static final String URL = "jdbc:postgresql://localhost:5432/bd_financas";
+    
+    
+    private static final String USER = "postgres"; 
+    private static final String PASS = "shottz"; 
 
     public static Connection conectar() throws SQLException {
-        // Essa linha é a que efetivamente "abre a porta" do banco
+        
         return DriverManager.getConnection(URL, USER, PASS);
     }
 
-    // --- CÓDIGO DE TESTE RÁPIDO ---
-    // Colocamos um main() aqui só para rodar esse arquivo isolado e ver se conecta
+    
     public static void main(String[] args) {
         try {
             Connection conn = conectar();
